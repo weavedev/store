2d0c2657ecf23d86fd51d9533dd6eb8e3626503c
Paul Gerarts <paul@gerarts.nl>
2020-01-15 16:16:26 +0100

    Add store package

---
+++ lib/defaultSaga.ts
@@ -0,0 +1,21 @@
+import { Action } from 'redux';
+import { ForkEffect, put, PutEffect, takeLatest } from 'redux-saga/effects';
+
+export function loadDefaultSaga(): void {
+    // Default ready saga
+    window.storeSagas.$$init = function* watchInit(): IterableIterator<ForkEffect> {
+        yield takeLatest('@@SAGA_INIT', function* (): IterableIterator<PutEffect> {
+            yield put({ type: '@@SAGA_READY' });
+        });
+    };
+}
+
+declare global {
+    interface StoreActionsMap {
+        $$init: Action<'@@SAGA_INIT'> | Action<'@@SAGA_READY'>;
+    }
+
+    interface StoreSagasMap {
+        $$init(): IterableIterator<ForkEffect>;
+    }
+}
